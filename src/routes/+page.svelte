a717a16b2d0c003244903999725339b37f38f445
Gabriel Alves <ggaa@discente.ifpe.edu.br>
2026-06-01 18:46:46 -0300

    🎨 style: credits page finished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,37 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
+  import { goto } from '$app/navigation';
 
-	const gameTitle: string = "STRAIGHT";
-	const gameSubtitle: string = "DEATH";
+  const gameTitle = "STRAIGHT";
+  const gameSubtitle = "DEATH";
 
-	function showAlert(): void {
-		alert("Still on development!");
-	}
+  function showAlert() {
+    alert("Still on development!");
+  }
 
-     function play(): void {
-	     goto('/play');
-	}
+  function play() {
+    goto('/play');
+  }
 
-    function credits(): void {
-	     goto('/credits');
-	}
+  function credits() {
+    goto('/credits');
+  }
 </script>
 
 <div class="menu-background">
-	<div class="container">
-		<h1 class="title">{gameTitle}</h1>
-		<h2 class="subtitle">{gameSubtitle}</h2>
-
-		<button class="play" on:click={play}>
-			<img src="/images/Menu/play.svg" class="play-icon" alt="Play">
-			<p>PLAY</p>
-		</button>
-
-		<button class="credits" on:click={credits}>
-			<img src="/images/Menu/credits.svg" class="credits-icon" alt="Credits">
-			<p>CREDITS</p>
-		</button>
-
-		<button class="leave" on:click={showAlert}>
-			<img src="/images/Menu/leave.svg" class="leave-icon" alt="Leave">
-			<p>LEAVE GAME</p>
-		</button>
-	</div>
+  <div class="container">
+    <h1 class="title">{gameTitle}</h1>
+    <h2 class="subtitle">{gameSubtitle}</h2>
+    <button class="play" on:click={play}>
+      <img src="/images/Menu/play.svg" class="play-icon" alt="Play" />
+      <p>PLAY</p>
+    </button>
+    <button class="credits" on:click={credits}>
+      <img src="/images/Menu/credits.svg" class="credits-icon" alt="Credits" />
+      <p>CREDITS</p>
+    </button>
+    <button class="leave" on:click={showAlert}>
+      <img src="/images/Menu/leave.svg" class="leave-icon" alt="Leave" />
+      <p>LEAVE GAME</p>
+    </button>
+  </div>
 </div>
--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,12 +1,65 @@
-<h1>Sobre</h1>
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  function handleKey(event: KeyboardEvent) {
+    if (event.key === 'Escape') goto('/');
+  }
+</script>
 
-<img class="sobre" src="/images/Credits/logo-ifpe.svg" alt="logo IPFE"/>
+<svelte:window on:keydown={handleKey} />
 
-<p>
-    Autor: Allan Diego Silva Lima
-</p>
-<p>
-  <a href="allan.lima@igarassu.ifpe.edu.br">allan.lima@igarassu.ifpe.edu.br</a>
-</p>
+<div class="credits-background">
+  <div class="content">
+    <h1 class="credits-title">CREDITS</h1>
+    <img src="/images/Credits/logo-ifpe.svg" class="logo-ifpe" />
+    <p class="instituto">INSTITUTO FEDERAL</p>
+    <p class="estado">Pernambuco</p>
 
-<a class="menu" href="/">Voltar ao Menu</a>
+    <!-- Box Desenvolvimento -->
+    <div class="box">
+      <p class="dev">DEVELOPMENT</p>
+
+      <div class="team-row">
+    <div class="member">
+        <img src="/images/Credits/fabricio.svg" class="fabricio-pic" />
+        <p class="fabricio">FABRÍCIO MOURA</p>
+        <p class="role">Pixel artist</p>
+    </div>
+    <div class="member">
+        <img src="/images/Credits/jp.svg" class="joao-pic" />
+        <p class="joao">JOÃO PEDRO</p>
+        <p class="role">Programmer</p>
+    </div>
+    <div class="member">
+        <img src="/images/Credits/matias.svg" class="matias-pic" />
+        <p class="matias">MATIAS LOPES</p>
+        <p class="role">Programmer</p>
+    </div>
+</div>
+
+<div class="team-row">
+    <div class="member">
+        <img src="/images/Credits/fagner.svg" class="fagner-pic" />
+        <p class="fagner">FÁGNER MOURA</p>
+        <p class="role">Game Designer</p>
+    </div>
+    <div class="member">
+        <img src="/images/Credits/gabriel.svg" class="gabriel-pic" />
+        <p class="gabriel">GABRIEL GALDINO</p>
+        <p class="role">Programmer</p>
+    </div>
+</div>
+    </div>
+
+    <!-- Box Agradecimentos -->
+    <div class="box-thanks">
+      <h1 class="thanks">ACKNOWLEDGEMENTS</h1>
+      <p class="thanks2">
+        We would like to thank everyone who rooted for us, supported us, and believed in this project.
+      </p>
+    </div>
+    <div class="esc">
+      <img src="/images/Credits/esc.svg" />
+      <p>PRESS <span class="key">ESC</span> TO RETURN</p>
+    </div>
+  </div>
+</div>
